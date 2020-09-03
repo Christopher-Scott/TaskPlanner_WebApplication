74028d969bdd8eb846c56bf94bf8daaeda8f937a
Christopher-Scott <44304416+Christopher-Scott@users.noreply.github.com>
2020-09-03 16:46:01 -0400

    Added window click functionality for dropdowns

--- web/js/drop.js
+++ web/js/drop.js
@@ -1,26 +1,42 @@
-function drop(elem) {
-    //console.log(elem)
-    // Because there is only one child with class drop_content
-    // the first element is the one we want
-    var content = elem.parentElement.getElementsByClassName("drop_content")[0];
-    //console.log(content);
-    
-    if(content.classList.contains("show")){
-        hide(content);
+window.onclick = function drop(event) {
+    var clickedElem = event.target;
+    var dropContentList = document.getElementsByClassName("drop_content");
+
+    // user clicked on a dropdown header
+    if (clickedElem.classList.contains("drop_header")) {
+        var content = clickedElem.parentElement.getElementsByClassName("drop_content")[0];
+        
+        
+        if(content.classList.contains("show")){
+            hide(content); // clicked dropdown is currently open, so hide it
+        }
+        else{
+            // close dropdowns
+            for (var i = 0; i < dropContentList.length; i++) {
+                hide(dropContentList[i]);
+            }
+            // Open the selected dropdown
+            show(content);
+        }
     }
-    else{
-        show(content);
+    // user clicked elsewhere on the page, close all the dropdowns
+    else {
+
+        for (var i = 0; i < dropContentList.length; i++) {
+            hide(dropContentList[i]);
+        }
     }
-    
-    function hide(elem){
+
+    function hide(elem) {
         elem.classList.remove("show");
         elem.classList.add("hide");
     }
-    
-    function show(elem){
+
+    function show(elem) {
         elem.classList.remove("hide");
         elem.classList.add("show");
     }
-}
+};
+
 
 
